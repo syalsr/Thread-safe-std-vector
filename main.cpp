26b32ad8928165ddde8cd1454bb201710c5a4810
volobuevrostislav <emil07102000@gmail.com>
2022-07-09 16:12:15 +0300

    yet another commit

--- main.cpp
+++ main.cpp
@@ -30,23 +30,34 @@ public:
 		safe_vector.pop_back();
 		return val;
 	}
+	iterator<random_access_iterator_tag, T> begin() { return safe_vector.begin(); }
+	iterator<random_access_iterator_tag, T> end() { return safe_vector.end(); }
+	size_t size() { return safe_vector.size(); }
+	T* operator[](int i)
+	{
+		return safe_vector.data() + i;
+	}
 private:
 	vector<T> safe_vector{};
 	mutex m{};
 	condition_variable cond_var{};
 };
 
+condition_variable cond_global_var{};
+mutex mg;
 template<typename T>
 void push(thread_safe_vector<T>& vec)
 {
+	this_thread::sleep_for(chrono::milliseconds(1000));
 	for (int i = 0; i < 1000; ++i)
 		vec.push_back(i);
+	cond_global_var.notify_one();
 }
 
 template<typename T>
 void pop(thread_safe_vector<T>& vec)
 {
-	for (int i = 0; i < 1000; ++i)
+	for (int i = 0; i < 100; ++i)
 		vec.pop_back();
 }
 
@@ -59,6 +70,11 @@ int main()
 	th1.join();
 	th2.join();
 
+	unique_lock<mutex> um{ mg };
+	cond_global_var.wait(um, [&vec] { return vec.size() == 900; });
+
+	for (int i = 0; i < vec.size(); ++i)
+		cout << *vec[i];
 }
 
 
